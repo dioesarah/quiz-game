a6932246083db20e38fc5e8b793085248eed81c0
dioesarah <projetorenner41@gmail.com>
2025-05-30 10:37:02 -0300

    update

---
+++ logica.js
@@ -0,0 +1,10 @@
+botaoIniciar = document.querySelector(".iniciar")
+botaoIniciar.addEventListener("click", iniciarJogo)
+
+function iniciarJogo() {
+  telaInicial = document.querySelector(".tela-inicial")
+  telaInicial.style.display = "none"
+  
+  telaPerg = document.querySelector(".tela-pergunta")
+  telaPerg.style.display = "block"
+}
