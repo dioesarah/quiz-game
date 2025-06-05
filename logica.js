b90cf7d13c603448fb9731b52feb9a70a7a13c1d
dio <projetorenner41@gmail.com>
2025-06-05 10:31:57 -0300

    atualizei meu quiz

--- logica.js
+++ logica.js
@@ -7,7 +7,7 @@ listaPerguntas = [
       "Elefante",
       "Girafa"
     ],
-    correta: 1
+    correta: 1 
   },
   {
     pergunta: "Qual a primeira capital do Brasil ?",
@@ -57,7 +57,25 @@ function iniciarJogo() {
 }
 
 function mostrarPergunta() {
-  pergunta = listaPerguntas[0]
-  
-  console.log(pergunta.pergunta)
+  objPergunta = listaPerguntas[0];
+
+  textoPergunta = document.createTextNode(objPergunta.pergunta);
+  divPergunta = document.querySelector(".pergunta");
+  divPergunta.appendChild(textoPergunta); 
+
+  txtOpcao1 = document.createTextNode(objPergunta.opcoes[0]);
+  botao0 = document.querySelector(".opcao0");
+  botao0.appendChild(txtOpcao1);
+
+  txtOpcao2 = document.createTextNode(objPergunta.opcoes[1]);
+  botao0 = document.querySelector(".opcao1");
+  botao0.appendChild(txtOpcao2);
+
+  txtOpcao3 = document.createTextNode(objPergunta.opcoes[2]);
+  botao0 = document.querySelector(".opcao2");
+  botao0.appendChild(txtOpcao3);
+
+  txtOpcao4 = document.createTextNode(objPergunta.opcoes[3]);
+  botao0 = document.querySelector(".opcao3");
+  botao0.appendChild(txtOpcao4);
 }
