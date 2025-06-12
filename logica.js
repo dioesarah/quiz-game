db43d4f10c27c0b33518ed2b9fdc68cdc09aa832
dioesarah <projetorenner41@gmail.com>
2025-06-12 16:33:29 -0300

    update

--- logica.js
+++ logica.js
@@ -5,7 +5,7 @@ listaPerguntas = [
     correta: 1 
   },
   {
-    pergunta: "Qual a primeira capital do Brasil ?",
+    pergunta: "Qual foi a  primeira capital do Brasil ?",
     opcoes: ["Groaíras","Brasília","Salvador","Rio de Janeiro"],
     correta: 2
   },
@@ -38,9 +38,17 @@ botao1 = document.querySelector(".opcao1");
 botao2 = document.querySelector(".opcao2");
 botao3 = document.querySelector(".opcao3");
 
+perguntaAtual = 0
+
 telaAcertou.addEventListener("click", () => {
   telaAcertou.style.display = "none"
   iniciarJogo()
+})
+
+telaErrou.addEventListener("click", () => {
+  telaErrou.style.display = "none"
+  telaInicial.style.display = "block"
+  perguntaAtual = 0
 })
 
 botao0.addEventListener("click", () => {
@@ -83,30 +91,40 @@ function iniciarJogo() {
 }
 
 function mostrarPergunta() {
-  objPergunta = listaPerguntas[0];
+  objPergunta = listaPerguntas[perguntaAtual];
 
   textoPergunta = document.createTextNode(objPergunta.pergunta);
+  divPergunta.innerHTML = ""
   divPergunta.appendChild(textoPergunta); 
 
   txtOpcao1 = document.createTextNode(objPergunta.opcoes[0]);
+  botao0.innerHTML = ""
   botao0.appendChild(txtOpcao1);
 
   txtOpcao2 = document.createTextNode(objPergunta.opcoes[1]);
+  botao1.innerHTML = ""
   botao1.appendChild(txtOpcao2);
 
   txtOpcao3 = document.createTextNode(objPergunta.opcoes[2]);
+  botao2.innerHTML = ""
   botao2.appendChild(txtOpcao3);
 
   txtOpcao4 = document.createTextNode(objPergunta.opcoes[3]);
+  botao3.innerHTML = ""
   botao3.appendChild(txtOpcao4);
 }
 
 function mostrarAcertou() {
   telaPergunta.style.display = "none"
   telaAcertou.style.display = "block"
+  perguntaAtual ++
 }
 
 function mostrarErrou() {
   telaPergunta.style.display = "none"
   telaErrou.style.display = "block"
+}
+
+function mostrarGanhou() {
+  tela
 }
